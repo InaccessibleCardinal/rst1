776fa82cd1f422d3b81acaffc7c8044ae0d216e3
ken <kenneth.landsbaum@projekt202.com>
2023-01-14 14:54:37 -0600

    updates

--- ctrl/responses_test.go
+++ ctrl/responses_test.go
@@ -1,8 +1,10 @@
 package ctrl
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -10,10 +12,16 @@ func TestSuccessResponse(t *testing.T) {
 
 	rec := httptest.NewRecorder()
 	mockResp := map[string]int{"problems": 99}
+	expected := []byte(`{"problems":99}`)
 
 	SendResponse(rec, mockResp, http.StatusOK)
-	if status := rec.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
+	if status := rec.Result().StatusCode; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %d want %d",
 			status, http.StatusOK)
+	}
+	defer rec.Result().Body.Close()
+	actual, _ := io.ReadAll(rec.Result().Body)
+	if strings.Compare(string(actual), string(expected)) != 1 {
+		t.Errorf("expected %s but got %s", string(actual), string(expected))
 	}
 }
--- ctrl/userController_test.go
+++ ctrl/userController_test.go
@@ -1,8 +1,11 @@
 package ctrl
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -12,6 +15,14 @@ var (
 	testController        = NewUserController(&testSuccessService)
 	testFailureController = NewUserController(&testFailureService)
 )
+
+func verifyBody(t *testing.T, actual []byte, expected []byte) {
+	actualStr := string(actual)
+	expectedStr := string(expected)
+	if strings.Compare(actualStr, expectedStr) != 1 {
+		t.Errorf("expected %s but got %s", expectedStr, actualStr)
+	}
+}
 
 func TestGetUsersSuccess(t *testing.T) {
 	req, err := http.NewRequest("GET", "/users", nil)
@@ -25,6 +36,10 @@ func TestGetUsersSuccess(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
+	resp := rec.Result()
+	body, _ := io.ReadAll(resp.Body)
+	j, _ := json.Marshal(mockUsers)
+	verifyBody(t, body, j)
 }
 
 func TestGetUserFailure(t *testing.T) {
